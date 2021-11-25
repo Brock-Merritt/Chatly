f65513f80282db873490e9b5d132db22c9346ccf
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-11-24 20:34:03 -0500

    added MVC

--- server.js
+++ server.js
@@ -29,3 +29,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
