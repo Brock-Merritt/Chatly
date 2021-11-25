80b349f66ce27769228f3b071163e19cc18b2832
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-11-24 19:30:36 -0500

    adding dependencies

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-java
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
