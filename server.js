a55381b4ef0c0bdc0ae9fd3dd4a37d68e17824c3
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-11-24 19:52:59 -0500

    APP port listening

--- server.js
+++ server.js
@@ -2,9 +2,30 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const sequelize = require('sequelize');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+const hbs = exphbs.create({});
+
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// sequelize.sync({ force: false}).then(() => {
+//     app.listen(PORT, () => console.log('now listening'));
+// });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
