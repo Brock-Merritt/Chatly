0c7884bc800f2437cb8973ad11801883d2e059d5
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-11-28 18:43:05 -0500

    socket.io starting

--- server.js
+++ server.js
@@ -4,15 +4,29 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const sequelize = require('sequelize');
 const routes = require('./controllers/');
+const { Server} = require('socket.io')
+const { createServer } = require('http');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //hello
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 const hbs = exphbs.create({});
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */});
+
+console.log(`test`);
+
+io.on('connection', (socket) => {
+  socket.emit("hello", "world");
+  console.log(socket.emit);
+  console.log(`test io.on`);
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
 
 
 
@@ -27,9 +41,10 @@ app.use(express.static(path.join(__dirname,'public')));
 // sequelize.sync({ force: false}).then(() => {
 //     app.listen(PORT, () => console.log('now listening'));
 // });
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 app.get('/', (req, res) => {
   res.render('homepage');
