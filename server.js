edd0311c779a210c58bb8fa8b6c861c27162f003
detrasmental <detra.gordon76@gmail.com>
2021-11-30 19:18:52 -0500

    new stuff

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const sequelize = require('sequelize');
 const routes = require('./controllers/');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
 
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -31,24 +38,53 @@ const socketio = require('socket.io');
 const io = socketio(server);
 
 //connection from browser
-io.on('connection', (socket) => {
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
  
 //Current user connects
-  socket.emit('message', 'This works!');
+  socket.emit('message',formatMessage('This is CHATly!'));
 
 // When a new user connects
-  socket.broadcast.emit('message', 'User has join chat');
+socket.broadcast
+.to(user.room)
+.emit(
+  'message',
+  formatMessage(`${user.username} has joined the chat`)
+);
 
-//A user disconnects
- // socket.on('disconnect', () => {
- //   io.omit('message', 'User has left the chat');
-//  });
+io.to(user.room).emit('roomUsers', {
+  room: user.room,
+  users: getRoomUsers(user.room)
+});
+});
 
   //catch chats
-  socket.on('chatMessage', (msg) => {
-    io.emit('chatMessage', msg);
-  })
-});
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+      const user = userLeave(socket.id);
+  
+      if (user) {
+        io.to(user.room).emit(
+          'message',
+          formatMessage(`${user.username} has left the chat`)
+        );
+  
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+        });
+      }
+    });
+  });
 
 // httpServer.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
