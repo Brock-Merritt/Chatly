6f0f6e6915fb900863dd9126e0b924b291d8f49a
caseylister <caseynlister@gmail.com>
2021-12-01 18:45:58 -0500

    js hbs connection

---
+++ public/js/dashboard.js
@@ -0,0 +1,14 @@
+const usernameProf = document.getElementById('username');
+const emailProf = document.getElementById('email');
+
+
+function showUsername(username){
+    usernameProf.innerText = username;
+  }
+
+
+function showEmail(email){
+    emailProf.innerText = email;
+}
+
+
